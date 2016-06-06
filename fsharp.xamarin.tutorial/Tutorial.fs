c2e929f7707d52ded1a6ed61eb74de6963d2516c
Roland Tritsch <roland.tritsch@gonitro.com>
2016-06-06 11:04:24 +0100

    Playing with Integers

--- fsharp.xamarin.tutorial/Tutorial.fs
+++ fsharp.xamarin.tutorial/Tutorial.fs
@@ -59,11 +59,14 @@ module Integers =
     /// A list of the numbers from 0 to 99
     let sampleNumbers = [ 0 .. 99 ]
  
-    /// A list of all tuples containing all the numbers from 0 to 99 and their squares
+    // The next line prints the list of numbers, using %A for generic printing
+    printfn "The list of numbers from 0 to 99 is:\n%A" sampleNumbers
+ 
+    /// A list of tuples containing the numbers from 0 to 99 and their squares
     let sampleTableOfSquares = [ for i in 0 .. 99 -> (i, i*i) ]
  
-    // The next line prints a list that includes tuples, using %A for generic printing
-    printfn "The table of squares from 0 to 99 is:\n%A" sampleTableOfSquares
+    // The next line prints the list of tuples, using %A for generic printing
+    printfn "The list of squares from 0 to 99 is:\n%A" sampleTableOfSquares
  
  
 module BasicFunctions =
