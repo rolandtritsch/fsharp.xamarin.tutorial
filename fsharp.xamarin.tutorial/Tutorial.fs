4934a16cc79d4f24d48582fce60a46896139c5d6
Roland Tritsch <roland.tritsch@gonitro.com>
2016-06-08 07:32:13 +0100

    Playing with Sequences

--- fsharp.xamarin.tutorial/Tutorial.fs
+++ fsharp.xamarin.tutorial/Tutorial.fs
@@ -308,50 +308,55 @@ module Arrays = begin
  end
 
 // ---------------------------------------------------------------
-//         Sequences
+// Sequences
 // ---------------------------------------------------------------
 
- 
-module Sequences =
-
+module Sequences = begin
     // Sequences are evaluated on-demand and are re-evaluated each time they are iterated.
     // An F# sequence is an instance of a System.Collections.Generic.IEnumerable<'T>,
     // so Seq functions can be applied to Lists and Arrays as well.
 
-    /// The empty sequence
+    // The empty sequence
     let seq1 = Seq.empty
+    let seq2 = seq { 
+        yield "hello"
+        yield "world"
+        yield "and"
+        yield "hello"
+        yield "world"
+        yield "again" 
+    }
 
-    let seq2 = seq { yield "hello"; yield "world"; yield "and"; yield "hello"; yield "world"; yield "again" }
+    // another array containing only the words "hello" and "world"
+    let seq3 = seq { 
+        for word in seq2 do
+            if word.Contains("l") then yield word
+        done
+    }
+    printfn "Seq of words: %A" seq2
+    printfn "Seq of words with an 'l' in it: %A" seq3
 
-    let numbersSeq = seq { 1 .. 1000 }
+    let numbersSeq = seq { 1 .. 10 }
+    let evenNumbers = Seq.init(10)(fun n -> n * 2)
+    printfn "Even numbers: %A" evenNumbers
 
-    /// another array containing only the words "hello" and "world"
-    let seq3 =
-        seq { for word in seq2 do
-                  if word.Contains("l") then
-                      yield word }
-
-
-    let evenNumbers = Seq.init 1001 (fun n -> n * 2)
-
+    // An infinite sequence which is a random walk
+    // Use yield! to return each element of a subsequence, similar to IEnumerable.SelectMany.
     let rnd = System.Random()
-
-    /// An infinite sequence which is a random walk
-    //  Use yield! to return each element of a subsequence, similar to IEnumerable.SelectMany.
-
-    let rec randomWalk x =
-        seq { yield x
-              yield! randomWalk (x + rnd.NextDouble() - 0.5) }
+    let rec randomWalk(x) = seq { 
+        yield x
+        yield! randomWalk(x + rnd.NextDouble() - 0.5) 
+    }
  
-
-    let first100ValuesOfRandomWalk =
+    let first100ValuesOfRandomWalk = begin
         randomWalk 5.0
         |> Seq.truncate 100
         |> Seq.toList
-
+    end
+end
 
 // ---------------------------------------------------------------
-//         Recursive functions
+// Recursive functions
 // ---------------------------------------------------------------
 
 
