615bde96cdb6d231fb9c12a4cd7387e96646d70d
Roland Tritsch <roland.tritsch@gonitro.com>
2016-06-11 17:34:30 +0100

    Playing with Lists

--- fsharp.xamarin.tutorial/ForFun.fs
+++ fsharp.xamarin.tutorial/ForFun.fs
@@ -10,13 +10,15 @@
 let myInt = 5
 let myFloat = 3.14
 let myString = "hello"   //note that no types needed
+printfn("%s world with %d and %f")(myString)(myInt)(myFloat)
 
 // ======== Lists ============
 let twoToFive = [2;3;4;5]        // Square brackets create a list with
                                  // semicolon delimiters.
 let oneToFive = 1 :: twoToFive   // :: creates list with new 1st element
-// The result is [1;2;3;4;5]
+printfn("OneToFive: %A")(oneToFive)
 let zeroToFive = [0;1] @ twoToFive   // @ concats two lists
+printfn("ZeroToFive: %A")(zeroToFive)
 
 // IMPORTANT: commas are never used as delimiters, only semicolons!
 
