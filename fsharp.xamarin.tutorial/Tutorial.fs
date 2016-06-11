b417044ee9b07c9e028705adee23387dbff5bee2
Roland Tritsch <roland.tritsch@gonitro.com>
2016-06-11 16:21:21 +0100

    Playing with Options

--- fsharp.xamarin.tutorial/Tutorial.fs
+++ fsharp.xamarin.tutorial/Tutorial.fs
@@ -507,33 +507,29 @@ end
 // Option types
 // ---------------------------------------------------------------
 
-module OptionTypes = 
-    /// Option values are any kind of value tagged with either 'Some' or 'None'.
-    /// They are used extensively in F# code to represent the cases where many other
-    /// languages would use null references.
+module OptionTypes = begin 
+    // Option values are any kind of value tagged with either 'Some' or 'None'.
+    // They are used extensively in F# code to represent the cases where many other
+    // languages would use null references.
+    type Customer = { zipCode: decimal option }
 
-    type Customer = { zipCode : decimal option }
-
-    /// Abstract class that computes the shipping zone for the customer's zip code,
-    /// given implementations for the 'GetState' and 'GetShippingZone' abstract methods.
-
+    // Abstract class that computes the shipping zone for the customer's zip code,
+    // given implementations for the 'GetState' and 'GetShippingZone' abstract methods.
     [<AbstractClass>]
+    type ShippingCalculator = class
+        abstract GetState: decimal -> string option
+        abstract GetShippingZone: string -> int
 
-    type ShippingCalculator =
-
-        abstract GetState : decimal -> string option
-
-        abstract GetShippingZone : string -> int
-
-        /// Return the shipping zone corresponding to the customer's ZIP code
-        /// Customer may not yet have a ZIP code or the ZIP code may be invalid
-        member this.CustomerShippingZone(customer : Customer) =
-
+        // Return the shipping zone corresponding to the customer's ZIP code
+        // Customer may not yet have a ZIP code or the ZIP code may be invalid
+        member this.CustomerShippingZone(customer: Customer) = begin 
             customer.zipCode |> Option.bind this.GetState |> Option.map this.GetShippingZone
+        end
+    end
+end
 
- 
 // ---------------------------------------------------------------
-//         Pattern matching
+// Pattern matching
 // ---------------------------------------------------------------
 
 module PatternMatching =
