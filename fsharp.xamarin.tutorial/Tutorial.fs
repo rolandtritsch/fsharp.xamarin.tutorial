3e15fba3df12a1b0c3180ba70d3a54f3bdeed61f
Roland Tritsch <roland.tritsch@gonitro.com>
2016-06-11 17:02:09 +0100

    Playing with Units of Measure

--- fsharp.xamarin.tutorial/Tutorial.fs
+++ fsharp.xamarin.tutorial/Tutorial.fs
@@ -549,8 +549,8 @@ module PatternMatching = begin
     let rec countReports(emp: Employee) = begin
         1 + match emp with
             | Engineer(id) -> 0
-            | Manager(id, reports) -> reports |> List.sumBy countReports
-            | Executive(id, reports, assistant) -> (reports |> List.sumBy countReports) + countReports assistant
+            | Manager(id, reports) -> reports |> List.sumBy(countReports)
+            | Executive(id, reports, assistant) -> (reports |> List.sumBy countReports) + countReports(assistant)
     end
 
     // Find all managers/executives named "Dave" who do not have any reports
@@ -564,30 +564,24 @@ module PatternMatching = begin
 end
 
 // ---------------------------------------------------------------
-//         Units of measure
+// Units of measure
 // ---------------------------------------------------------------
 
-module UnitsOfMeasure =
+module UnitsOfMeasure = begin
     // Code can be annotated with units of measure when using F# arithmetic over numeric types
-
     open Microsoft.FSharp.Data.UnitSystems.SI.UnitNames
 
     [<Measure>]
     type mile =
-
-        /// Conversion factor mile to meter: meter is defined in SI.UnitNames
-
+        // Conversion factor mile to meter: meter is defined in SI.UnitNames
         static member asMeter = 1600.<meter/mile>
-
  
     let d  = 50.<mile>          // Distance expressed using imperial units
     let d' = d * mile.asMeter   // Same distance expressed using metric system
 
     printfn "%A = %A" d d'
     // let error = d + d'       // Compile error: units of measure do not match
-
-
- 
+end
 
 // ---------------------------------------------------------------
 //         Parallel array programming
