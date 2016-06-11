87bff8f6effa496035ad5962e4bb82912fac4702
Roland Tritsch <roland.tritsch@gonitro.com>
2016-06-11 19:06:53 +0100

    Adding HackerPhrase

---
+++ HackerPhrase/HackerPhrase.fs
@@ -0,0 +1,7 @@
+﻿[<EntryPoint>]
+let main (args) = begin
+    let phrase = args.[0]
+    let hacker = phrase.Replace('o', '0').Replace('l', '1').Replace('e', '3')
+    printfn "%s" hacker
+    0
+end
