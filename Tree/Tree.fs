5c263f21933c4dc4f64cb0120a521ad86ba2bd64
Roland Tritsch <roland.tritsch@gonitro.com>
2016-06-12 07:27:34 +0100

    Adding Tree

---
+++ Tree/Tree.fs
@@ -0,0 +1,14 @@
+﻿type Tree =
+    | Node of left: Tree option * right: Tree option * key: int
+
+[<EntryPoint>]
+let main (args) = begin
+    let tree = Node(Some(Node(Some(Node(None, None, 1)), Some(Node (None, None, 3)), 2)), Some(Node (None, None, 5)), 4)
+    let rec traverse tree = 
+        match tree with 
+        | Node (Some(left), Some(right), key) -> traverse left; traverse right; printfn "Node (%d)" key
+        | Node (None, None, key) -> printfn "Leaf [%d]" key
+        | _ -> printfn "ERROR"
+    traverse tree
+    0
+end
