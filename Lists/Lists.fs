94f4230d256767b842a4b46812f0229c86afb34b
Roland Tritsch <roland.tritsch@gonitro.com>
2016-06-12 06:45:59 +0100

    Adding Lists

---
+++ Lists/Lists.fs
@@ -0,0 +1,13 @@
+﻿[<EntryPoint>]
+let main (args) = begin
+    let size = (int32) args.[0]
+        
+    let list = 
+        [ 0..size ]
+        |> List.filter (fun x -> x % 2 = 0)
+        |> List.map (fun x -> x * 2)
+    
+    let sum = list |> List.sum
+    printfn "sum(%d) of %A -> %d" size list sum
+    0
+end
