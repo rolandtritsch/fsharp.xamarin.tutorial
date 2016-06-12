80667421ac2379b0db309b09a0c7b3462db92ac9
Roland Tritsch <roland.tritsch@gonitro.com>
2016-06-12 06:49:04 +0100

    Adding Matching

---
+++ Matching/Matching.fs
@@ -0,0 +1,19 @@
+﻿let numbers = [1..10]
+
+type Colors =
+    | Red
+    | Green
+    | Blue
+
+[<EntryPoint>]
+let main args = begin
+    let colors = [| Red; Green; Blue |]
+    let currentColor = colors.[System.Random().Next() % colors.Length]
+    let strColor =
+        match currentColor with
+        | Red -> "Red"
+        | Green -> "Green"
+        | Blue -> "Blue"
+    printfn "Color: %A %s" currentColor strColor
+    0
+end
