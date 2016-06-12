0cc5330cea1980343d5ad7a5a7a8b9b2519c4b71
Roland Tritsch <roland.tritsch@gonitro.com>
2016-06-12 06:38:25 +0100

    Adding HigherOrderFunction

---
+++ HigherOrderFunction/HigherOrderFunction.fs
@@ -0,0 +1,9 @@
+﻿[<EntryPoint>]
+let main (args) = begin
+    let a = (int32) args.[0]
+    let b = (int32) args.[1]
+    let isBigger test a b = test a b
+    let myTest a b = a > b
+    printf "%d isbigger than %d: %b" a b (isBigger myTest a b)
+    0
+end
