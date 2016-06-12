f17a582f0dc86e62ec045c32441afee165b300d8
Roland Tritsch <roland.tritsch@gonitro.com>
2016-06-12 07:30:45 +0100

    Add Unions

---
+++ Unions/Unions.fs
@@ -0,0 +1,19 @@
+﻿type State =
+    | Up
+    | Down
+    | GoingUp
+    | GoingDown
+    | Unknown
+
+[<EntryPoint>]
+let main (args) = begin
+    let allStates = [| Up; Down; GoingUp; GoingDown; Unknown |]
+    let currentState = allStates.[ System.Random().Next() % allStates.Length ]
+    match currentState with 
+    | Up -> printfn "Up"
+    | Down -> printfn "Down"
+    | GoingUp -> printfn "GoingUp"
+    | GoingDown -> printfn "GoingDown"
+    | Unknown -> printfn "Unkown"
+    0
+end
